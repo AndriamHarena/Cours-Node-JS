955c5f7e8975fe774bf1482fed8311c851aa6fa6
AndriamHarena <andriamananjaramandimbyharena@gmail.com>
2025-09-16 12:26:04 +0200

    feat: implement delete route for monument management

---
+++ src/routes/deleteMonument.route.js
@@ -0,0 +1,24 @@
+const { MonumentModel } = require('../db/sequelize');
+
+module.exports = (app) => {
+    app.delete('/monuments/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+
+        return MonumentModel.findByPk(id).then(monument => {
+            if (monument === null) {
+                const message = `Le monument avec l'ID ${id} n'existe pas.`;
+                return res.status(404).json({ message, data: null });
+            }
+
+            return MonumentModel.destroy({
+                where: { id: id }
+            }).then(() => {
+                const message = `Le monument avec l'id ${id} a bien été supprimé.`;
+                res.json({ message, data: monument });
+            });
+        }).catch(error => {
+            const message = `Une erreur s'est produite lors de la recherche du monument : ${error}`;
+            res.status(500).json({ message, data: null });
+        });
+    });
+};
